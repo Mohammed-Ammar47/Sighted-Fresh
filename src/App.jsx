fe56f20daa5eec180de294d83bf4fd2dca49ccdc
unknown <mohamedpocker2016@hotmail.com>
2023-08-07 19:45:44 +0100

    handeling navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router , Routes , Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Store from './pages/Store'
+import Deals from './pages/Deals'
+import Register from './pages/Register'
+import SignIn from './pages/SignIn'
 import Header from './components/Header'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
 
 function App() {
   return (
@@ -10,6 +16,12 @@ function App() {
     <Header />
     <Routes>
     <Route path='/' element={<Home />} />
+    <Route path='/store' element={<Store />} />
+    <Route path='/deals' element={<Deals />} />
+    <Route path='/cart' element={<Cart />} />
+    <Route path='/register' element={<Register />} />
+    <Route path='/sign-in' element={<SignIn />} />
+    <Route path='/:productId' element={<Product />} />
     </Routes>
     </Router>
     </>
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 
 export default function SignIn() {
   return (
-    <div>SignIn</div>
+    <>
+    {/* <main className='  flex justify-center items-center   '>
+    <div className='w-full flex flex-col  justify-center items-center  sm:mx-auto sm:w-full sm:max-w-sm my-5 p-5 lg:mx-auto   shadow-lg max-w-sm lg:max-w-3xl bg-[#F1F1F1] mx-auto mt-14 px-3 rounded-xl sm:rounded-3xl '>
+    <h2 className='flex flex-col font-semibold text-xl my-3'>Sign In</h2>
+    <form className='flex flex-col  mx-auto'>
+    
+    <input className='w-full mx-3 px-3 mb-4 py-2 text-base md:text-xl lg:text-2xl text-slate-900  rounded-md bg-white  border-0 outline-none transition ease-in-out shadow-sm  ring-inset focus:ring-2 focus:ring-inset focus:ring-red-600   '/>
+    <input className='w-full mx-3 px-3 mb-4 py-2 text-base md:text-xl lg:text-2xl text-slate-900  rounded-md bg-white  border-0 outline-none transition ease-in-out shadow-sm  ring-inset focus:ring-2 focus:ring-inset focus:ring-red-600   '/>
+    
+    </form>
+    </div>
+    </main> */}
+</>
   )
 }

c4cf04f5f5a42cbcc94013a6435a449f59ccdc79
unknown <mohamedpocker2016@hotmail.com>
2023-08-07 19:48:16 +0100

    handeling navbar2

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function SignIn() {
   return (
     <>
-    {/* <main className='  flex justify-center items-center   '>
+    <main className='  flex justify-center items-center   '>
     <div className='w-full flex flex-col  justify-center items-center  sm:mx-auto sm:w-full sm:max-w-sm my-5 p-5 lg:mx-auto   shadow-lg max-w-sm lg:max-w-3xl bg-[#F1F1F1] mx-auto mt-14 px-3 rounded-xl sm:rounded-3xl '>
     <h2 className='flex flex-col font-semibold text-xl my-3'>Sign In</h2>
     <form className='flex flex-col  mx-auto'>
@@ -13,7 +13,7 @@ export default function SignIn() {
     
     </form>
     </div>
-    </main> */}
+    </main>
 </>
   )
 }
